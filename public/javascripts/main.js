0d35130c3f9178d6f77e0468494dc25678b6e3e7
abekim <abekim0607@gmail.com>
2013-03-05 00:23:16 -0500

    mp3 features

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,26 @@
 $(function () {
-  if (player.getCurrentTime() == 6.000) {
+  window.sound = new Audio('/audio/goat.mp3');
+  setInterval(function () {
+    var current = player.getCurrentTime();
+    console.log(current, isBetween(current, 6, 2));
+    if (isBetween(current, 6, 2)) { toggle(true); } else { toggle(false); }
+  }, 200);
+});
+
+function isBetween(current, start, duration) {
+  return current > start && current < (start + duration);
+}
+
+function toggle(goat) {
+  if (goat) {
     player.mute();
     $('.video').css('display','none');
     $('.goat').css('display','block');
-    var sound = $('<embed autoplay="true" height="0" width="0" autoplay="true" />');
-    sound.attr('src', '/audio/goat.mp3');
-    $('#scream').html(sound);
+    window.sound.play();
+  } else {
+    player.unMute();
+    $('.video').css('display','block');
+    $('.goat').css('display','none');
+    window.sound.pause();
   }
-});
+}
